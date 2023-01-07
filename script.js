657ab33eaddb059666694b80ed7e568e7e069c61
julius-tamale <juliotamex@proton.me>
2023-01-07 22:20:40 +0200

    portifolio website under construction

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
 let string = 'Portifolio website still under construction. \n Visit back soon'; /* type your text here */
 let array = string.split("");
+let textEl = document.querySelector(".main-heading");
 let timer;
 
 function frameLooper () {
 	if (array.length > 0) {
-		document.querySelector(".main-heading").innerHTML += array.shift();
+		textEl.innerHTML += array.shift();
 	} else {
 		clearTimeout(timer);
 			}
